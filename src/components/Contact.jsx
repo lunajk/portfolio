75edcae1dc2aaa95bca51ae61e416f28d5d2bd0d
lunajk <vigneshwarisakthivel8@gmail.com>
2025-06-29 18:10:42 +0530

    next2 commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
   
   try {
-    const response = await fetch("http://localhost:8000/api/contact/", {
+    const response = await fetch("https://portfolio-backend-ic9s.onrender.com/api/contact/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
